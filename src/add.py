b5ed61c166ccc757b2cd3fde51bedfb2d24afced
Ritik <ritik@Ritik.localdomain>
2026-06-10 03:43:34 +0000

    Code change

--- src/add.py
+++ src/add.py
@@ -1,7 +1,19 @@
 def add(a, b):
     return a + b
-
-
+def sub(a, b):
+    if [a -gt b]
+    then
+	return a - b
+    break
+    fi
+    if [a -gt b]
+    then
+        return b - a
+    break
+    fi
 def test_add():
     assert add(1, 2) == 3
     assert add(1, -1) == 0
+def test_sub():
+    assert sub(1,2) == 1
+    assert sub(-1,-2) == 1
