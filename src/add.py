7189940d80ebbc809542cbfb11b3e9f1ff80a8a8
Ritik <ritik@Ritik.localdomain>
2026-06-10 03:48:57 +0000

    idk

--- src/add.py
+++ src/add.py
@@ -1,14 +1,13 @@
 def add(a, b):
     return a + b
+
 def sub(a, b):
-    if a>b:
-	return a - b
-    if b>a:
-        return b - a
+    return abs(a - b)
 
 def test_add():
     assert add(1, 2) == 3
     assert add(1, -1) == 0
+
 def test_sub():
-    assert sub(1,2) == 1
-    assert sub(-1,-2) == 1
+    assert sub(1, 2) == 1
+    assert sub(-1, -2) == 1
